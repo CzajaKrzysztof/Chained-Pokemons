cc09780c05ded6e4413f456f6818d5c23f1dd098
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-07 10:04:57 +0100

    Added Pokemons names in module pokemon_names.py. Implemented functions get_user_name() and get_pokemon_name() in file Chainde_Pokemons.py

--- Chained_Pokemons.py
+++ Chained_Pokemons.py
@@ -1,39 +1,27 @@
-pokemins = ['audino', 'bagon', 'baltoy', 'banette', 'bidoof', 'braviary', 'bronzor', 'carracosta', 'charmeleon',
-            'cresselia', 'croagunk', 'darmanitan', 'deino', 'emboar', 'emolga', 'exeggcute', 'gabite', 'girafarig',
-            'gulpin', 'haxorus', 'heatmor', 'heatran', 'ivysaur', 'jellicent', 'jumpluff', 'kangaskhan', 'kricketune',
-            'landorus', 'ledyba', 'loudred', 'lumineon', 'lunatone', 'machamp', 'magnezone', 'mamoswine', 'nosepass',
-            'petilil', 'pidgeotto', 'pikachu', 'pinsir', 'poliwrath', 'poochyena', 'porygon2', 'porygonz', 'registeel',
-            'relicanth', 'remoraid', 'rufflet', 'sableye', 'scolipede', 'scrafty', 'seaking', 'sealeo', 'silcoon',
-            'simisear', 'snivy', 'snorlax', 'spoink', 'starly', 'tirtouga', 'trapinch', 'treecko', 'tyrogue',
-            'vigoroth', 'vulpix', 'wailord', 'artortle', 'whismur', 'wingull', 'yamask']
+import pokemon_names
 
 
 def get_user_name(user):
-    while True:
-        try:
-            user_name = input("{0}: Please enter your name. ".format(user))
-        except ValueError:
-            print("Please use only letters.")
-        else:
-            return user_name
+    user_name = input("{0}: Please enter your name. ".format(user))
+    return user_name
 
 
 def get_pokemon_name(user_name):
-    while True:
-        try:
-            pokemon_name = input("Enter pokemon name: ")
-        except ValueError:
-            print("Please enter valid integers for the height.")
-        else:
-            break
+    pokemon_name = input("{0} enter pokemon name: ".format(user_name))
+    return pokemon_name
+
+
+def check_if_pokemon_correct(pokemon_name, pokemons_names_used, pokemon_names_list):
+    print(pokemon_names_list)
 
 
 def main():
     user_one_name = get_user_name("User one")
     user_two_name = get_user_name("User two")
+    pokemons_names_used = []
     while True:
-        get_pokemon_name("ser_one")
-        get_pokemon_name("ser_one")
+        pokemon_name = get_pokemon_name(user_one_name)
+        check_if_pokemon_correct(pokemon_name, pokemons_names_used, pokemon_names.pokemons_name_list)
 
 
 if __name__ == '__main__':
